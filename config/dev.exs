78485846061bdf48d85a3e7c168b872e4b4c3e84
lambda <lambda@swordfish.local>
2022-06-12 11:33:34 -0400

    tailwind: Add deps and configs

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :mork_borg, MorkBorgWeb.Endpoint,
   secret_key_base: "fE/BTXjJq5+rYCN4JzaVId1OyCBhuGqwmmt7rl7nWhqa0Zp+bYPWWM+2vsT6vAWP",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
