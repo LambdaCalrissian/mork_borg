baa372c73bd721f5ae95e6d8ada6044320cc5604
lambda <lambda@swordfish.local>
2022-06-20 19:47:20 -0400

    credo: Add dep

--- mix.exs
+++ mix.exs
@@ -53,7 +53,8 @@ defmodule MorkBorg.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
-      {:dialyxir, "~> 0.5.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
